853d5971168f58716ec6b0040b9d5f5a593164a4
Stephen_A <ankustephen99@gmail.com>
2025-10-08 11:58:43 -0400

    Implemented BufferedReader to add Product objects to arraylist from inventory.csv

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,13 +1,33 @@
 package com.pluralsight;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class SearchInventory {
-    public static void main(String[] args) {
-        ArrayList<Product> inventory = getInventory();
+    public static void main(String[] args) throws IOException {
+        try {
+            ArrayList<Product> inventory = getInventory();
+            String fileName = "inventory.csv";
 
-        for (Product product : inventory) {
-            System.out.println(product);
+            for (Product product : inventory) {
+                System.out.println(product);
+            }
+
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader inventoryBufReader = new BufferedReader(fileReader);
+            String line;
+            while((line = inventoryBufReader.readLine()) != null) {
+                String[] tokens = line.split("\\|");
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double price = Double.parseDouble(tokens[2]);
+                Product product = new Product(name, id, price);
+                System.out.println(product.toString());
+            }
+// close the stream and release the resources
+            inventoryBufReader.close();
+        } catch (Exception e) {
+            System.err.println("ERROR");
         }
     }
 
