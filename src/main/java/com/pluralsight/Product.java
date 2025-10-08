065b3b15eb4c1f3decc26d5ceb570b33d90cca51
Stephen_A <ankustephen99@gmail.com>
2025-10-08 11:33:08 -0400

    Set up exercise with instructor

---
+++ src/main/java/com/pluralsight/Product.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(String name, int id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
